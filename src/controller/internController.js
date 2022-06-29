b15b61929a1873531978ca0079bed49483fb9bb0
Krupa Bhatia <krupadbhatia0521@gmail.com>
2022-06-29 11:39:31 +0530

    validation

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,11 +1,36 @@
 const intern = require("../Model/internModel")
-const college =require("../Model/collegeModel")
+const college =require("../Model/collegeModel");
+const collegeModel = require("../Model/collegeModel");
 
 const createIntern = async function(req,res) {
    try { 
       let data = req.body;
        //************Here i am writtne validation*********** */
+       if(!Object.keys(data).length)
+       return res.status(400).send({status:false,message:"requst body is empty"})
 
+      //  if(!data.collegeName)
+      //  return res.status(400).send({status:false,message:"College name is Required"})
+
+      //  if(!data.collegeName.trim().match(/^[a-zA-Z]+$/))
+      //    return res.status(400).send({status:false,message:"name should be is correct format"})
+         
+      //    let checkCollegeName= await collegeModel.find({name:data.collegeName})
+      //    if(!checkCollegeName)
+      //    return res.status(400).send({status:false,message:"Your college is not registered"})
+
+         if(!data.name)
+         return res.status(400).send({status:false,message:"Name must be entered"})
+
+         if(!data.name.trim().match(/^[a-zA-Z,\s]*$/))
+         return res.status(400).send({status:false,message:"name should be is correct format"})
+
+         if(!email)
+         return res.status(400).send({status:false,message:"Email must be entered"})
+
+         if(!data.name.trim().match(/^[a-zA-Z0-9\+]*$/))
+         return res.status(400).send({status:false,message:"name should be is correct format"})
+         if(!email())
        
 
       //******************************************************** */
