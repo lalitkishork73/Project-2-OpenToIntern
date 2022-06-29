02c3a10b802ec37fdd9579d8e24d1928f0bcea43
lalitkishork73 <lalitkishork73@gmail.com>
2022-06-29 17:10:45 +0530

    all done

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const intern = require("../controller/internController");
 const college = require("../controller/collegeController");
 
-router.post("/functioup/college", college.createCollege);
+router.post("/functionup/college", college.createCollege);
 
 router.post("/functionup/interns",intern.createIntern);
 
