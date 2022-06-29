7f340366d7e85eb3ad3062133cea335901d57055
lalitkishork73 <lalitkishork73@gmail.com>
2022-06-29 16:30:06 +0530

    all done

--- src/Model/collegeModel.js
+++ src/Model/collegeModel.js
@@ -1,27 +1,29 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const CollegeSchema = new mongoose.Schema({
-     name : {
-         type : String,
-         required : true,
-         trim :true
-     },
-     fullName : {
-        type : String,
-        required : true,
-        trim : true
+const CollegeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    logoLink : 
-     {
-         type :String,
-        //  required :true,
-     },
-     isDeleted :{
-        type : Boolean,
-        default : false
-     }
-},{timestamps :true});
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    logoLink: {
+      type: String,
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const collegeModel = mongoose.model('college', CollegeSchema)
+const collegeModel = mongoose.model("college", CollegeSchema);
 
-module.exports = collegeModel
+module.exports = collegeModel;
