aa4cfc4f21683e062f329df102b16ea348820e29
DeepakSED <deepakcegin@gmail.com>
2022-06-29 12:02:09 +0530

    Code is Done

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -6,32 +6,48 @@ const createIntern = async function(req,res) {
    try { 
       let data = req.body;
        //************Here i am writtne validation*********** */
+         //  body validtion  
        if(!Object.keys(data).length)
        return res.status(400).send({status:false,message:"requst body is empty"})
+              
+            //  College Name Validation  
+        if(!data.collegeName)
+        return res.status(400).send({status:false,message:"College name is Required"})
 
-      //  if(!data.collegeName)
-      //  return res.status(400).send({status:false,message:"College name is Required"})
-
-      //  if(!data.collegeName.trim().match(/^[a-zA-Z]+$/))
-      //    return res.status(400).send({status:false,message:"name should be is correct format"})
+        if(!data.collegeName.trim().match(/^[a-zA-Z]+$/))
+          return res.status(400).send({status:false,message:"name should be is correct format"})
          
-      //    let checkCollegeName= await collegeModel.find({name:data.collegeName})
-      //    if(!checkCollegeName)
-      //    return res.status(400).send({status:false,message:"Your college is not registered"})
+       let checkCollegeName= await collegeModel.find({name:data.collegeName})
+         if(!checkCollegeName)
+          return res.status(400).send({status:false,message:"Your college is not registered"})
 
+          // Name  validation 
          if(!data.name)
          return res.status(400).send({status:false,message:"Name must be entered"})
 
          if(!data.name.trim().match(/^[a-zA-Z,\s]*$/))
          return res.status(400).send({status:false,message:"name should be is correct format"})
 
+
+          // Email id vaildation  
          if(!email)
          return res.status(400).send({status:false,message:"Email must be entered"})
+         if(!(/^[A-Za-z0-9_]{3,},@[A-Za-z]{3,},[.]{1}[A-Za-z.]{2,6}$/.test(data.email.trim())))
+         return res.status(400).send({status:false,message:"Email should be in correct format"})
+  
+        let isRegisteredEmail = await intern.find({email:data.email});
+        if(isRegisteredEmail.length)
+        return res.status(400).send({status:false,message:"Emailid alerady used or register"})
+    
+          //  mobile number  validation 
+        if(!data.mobile.trim().match(/^(\+\d{1,3}[- ]?)?\d{10}$/))   
+        return res.status(400).send({status:false,message:"Enter a valid mobile number"})
 
-         if(!data.name.trim().match(/^[a-zA-Z0-9\+]*$/))
-         return res.status(400).send({status:false,message:"name should be is correct format"})
-         if(!email())
-       
+        let isRegisteredMobile = await intern.find({mobile :data.mobile})
+        if(isRegisteredMobile.length)
+        return res.status(400).send({status:false,message:" mobile number is already register"})
+
+  
 
       //******************************************************** */
       delete data.collegeName._id
