a6f31e77f193cdcc7744770278bba39711d73e05
lalitkishork73 <lalitkishork73@gmail.com>
2022-06-29 12:23:55 +0530

    pull req

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -3,62 +3,105 @@ const { intern } = require("./internController");
 
 const createCollege = async function (req, res) {
   try {
-    
     // ********************************Validation****************************************************
     const data = req.body;
-    if(!Object.keys(data).length)
-     return res.status(400).send({ status :false, message: "You most enter data"})
+    if (!Object.keys(data).length)
+      return res
+        .status(400)
+        .send({ status: false, message: "You most enter data" });
 
-     if(!data.name) return res.status(400).send({status:false, message :"You mast be enter your name"})
-     if((!data.name.trim().match(/^[a-zA-Z]+$/)))
-        return res.status(400).send({status: false, meg :"Enter a valid name"})
+    if (!data.name)
+      return res
+        .status(400)
+        .send({ status: false, message: "You mast be enter your name" });
+    if (!data.name.trim().match(/^[a-zA-Z]+$/))
+      return res.status(400).send({ status: false, meg: "Enter a valid name" });
 
-     if(!data.fullName)   
-       return res.status(400).send({status :false,message : "Enter the Full name"})
+    if (!data.fullName)
+      return res
+        .status(400)
+        .send({ status: false, message: "Enter the Full name" });
 
-     if(!data.fullName.trim().match(/^[a-zA-Z,\-,\s]*$/))
-       return res.status(400).send({status : false,msg : "Enter a valid FullName"})
-     
-    if(!data.logoLink)
-       return res.status(400).send({status :false,message : "You Enter the logo link"})
-       
-       if (!data.logoLink.trim().match(/^(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%.\+#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.#?&//=]*)$/)) 
-            return res.status(400).send({status : false, message : "Enter the valid logo Link"})  
-     
-     //******************************************************************************************* */
+    if (!data.fullName.trim().match(/^[a-zA-Z,\-,\s]*$/))
+      return res
+        .status(400)
+        .send({ status: false, msg: "Enter a valid FullName" });
+
+    if (!data.logoLink)
+      return res
+        .status(400)
+        .send({ status: false, message: "You Enter the logo link" });
+
+    if (
+      !data.logoLink
+        .trim()
+        .match(
+          /^(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%.\+#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.#?&//=]*)$/
+        )
+    )
+      return res
+        .status(400)
+        .send({ status: false, message: "Enter the valid logo Link" });
+
+    //******************************************************************************************* */
     let createCollege = await collegeModel.create(data);
-         return res.status(201).send({ status: true, data: createCollege });
+    return res.status(201).send({ status: true, data: createCollege });
   } catch (err) {
-        return  res.status(500).send({ status: false, msg: err.message });
+    return res.status(500).send({ status: false, msg: err.message });
   }
-}
+};
 
+const collegeDetails = async function (req, res) {
+  try {
+    const info = req.query.collegeName;
+    if (!info)
+      return res
+        .status(400)
+        .send({ status: false, message: "Please Enter College Name" });
 
+    const college = await collegeModel.findOne({
+      name: info,
+      isDeleted: false,
+    });
+    if (!college)
+      return res
+        .status(404)
+        .send({
+          status: false,
+          message: "Did not found college with this name.",
+        });
 
+    const { name, fullName, logoLink } = college;
 
-const  getcollege = async function (req ,res){
-    let  getData = req.body 
+    const data = { name, fullName, logoLink };
 
-    if(!getData.collegeName)
-      return res.send.status(400).send({status :false , message : "You most be enter  the collge  Name"})
+    const collegeIdFromcollege = college._id;
 
-    let findCollge = await college.findOne({name : getData.collegeName })
-    
-    if(!findCollge)
-     return res.status(404).send({status :false , message : " Your college is not registeed with us"})
+    const internList = await internModel.find({
+      collegeId: collegeIdFromcollege,
+      isDeleted: false,
+    });
 
-    let collegeId = findCollge._id
-      let findIntern = await intern.find({ college :collegeId, isDeleted :false }).select({_id : 1,name : 1, email :1 , mobile:1})
+    if (internList.length == 0)
+      return res
+        .status(404)
+        .send({
+          status: false,
+          message: `We Did not Have Any Intern With ${info} College`,
+        });
 
-      return res.status(200).send ({ status : true,
-        data : {
-             "name" : findCollge.name,
-             "fullName" :findCollge.fullName,
-             "logoLink":findCollge.logoLink,
-             "interests":findIntern
-        }
-     })
-}
+    data["interests"] = internList;
+    res.status(200).send({ status: true, data: data });
+  } catch (err) {
+    res
+      .status(500)
+      .send({
+        status: false,
+        msg: "Server not responding",
+        error: err.message,
+      });
+  }
+};
 
 module.exports.createCollege = createCollege;
-module.exports.getcollege =getcollege;
+module.exports.collegeDetails = collegeDetails;
